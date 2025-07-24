9ad084f6e62e7b28d44b063c8560c0151eeb0fd1
ShivaRavinutala <152668909+ShivaRavinutala@users.noreply.github.com>
2025-07-24 12:46:21 -0700

    base scene created
    implemeted the following:
    
    - a base scene (20 x 200 tiles)
    - tileset loaded
    - zoom in / zoom out
    - scrolling left/right/up/down

---
+++ src/phaser/editorScene.ts
@@ -0,0 +1,159 @@
+import Phaser from "phaser";
+
+export class EditorScene extends Phaser.Scene {
+  private TILE_SIZE = 16;
+  private map!: Phaser.Tilemaps.Tilemap;
+  private groundLayer!: Phaser.Tilemaps.TilemapLayer;
+  private backgroundLayer!: Phaser.Tilemaps.TilemapLayer;
+  private gridGraphics!: Phaser.GameObjects.Graphics;
+
+  private minZoomLevel = 2.25;
+  private maxZoomLevel = 10;
+  private zoomLevel = 2.25;
+
+  constructor() {
+    super({ key: "editorScene" });
+  }
+
+  preload() {}
+
+  create() {
+    this.map = this.make.tilemap({ key: "defaultMap" });
+
+    const tileset = this.map.addTilesetImage(
+      "pewterPlatformerTileset",
+      "tileset",
+      16,
+      16,
+      0,
+      0,
+    )!;
+
+    this.backgroundLayer = this.map.createLayer(
+      "Background_Layer",
+      tileset,
+      0,
+      0,
+    )!;
+    this.groundLayer = this.map.createLayer("Ground_Layer", tileset, 0, 0)!;
+
+    this.cameras.main.setBounds(
+      0,
+      0,
+      this.map.widthInPixels,
+      this.map.heightInPixels,
+    );
+    this.cameras.main.centerOn(0, 0);
+    this.cameras.main.setZoom(this.zoomLevel);
+
+    // grid
+    this.gridGraphics = this.add.graphics();
+    this.gridGraphics.setDepth(10);
+    this.drawGrid();
+
+    // zoom in & zoom out
+    this.input.on(
+      "wheel",
+      (
+        pointer: Phaser.Input.Pointer,
+        gameObjects: Phaser.GameObjects.GameObject[],
+        deltaX: number,
+        deltaY: number,
+        deltaZ: number,
+      ) => {
+        if (deltaY > 0) {
+          this.zoomLevel = Phaser.Math.Clamp(
+            this.zoomLevel - 0.1,
+            this.minZoomLevel,
+            this.maxZoomLevel,
+          );
+        } else {
+          this.zoomLevel = Phaser.Math.Clamp(
+            this.zoomLevel + 0.1,
+            this.minZoomLevel,
+            this.maxZoomLevel,
+          );
+        }
+
+        this.cameras.main.setZoom(this.zoomLevel);
+      },
+    );
+
+    // scrolling
+    let isDragging = false;
+    let dragStartPoint = new Phaser.Math.Vector2();
+
+    this.input.on("pointerdown", (pointer: Phaser.Input.Pointer) => {
+      if (pointer.rightButtonDown()) return; // Optional: allow only left-click
+      isDragging = true;
+      dragStartPoint.set(pointer.x, pointer.y);
+    });
+
+    this.input.on("pointerup", () => {
+      isDragging = false;
+    });
+
+    this.input.on("pointermove", (pointer: Phaser.Input.Pointer) => {
+      if (!isDragging) return;
+
+      const dragX = dragStartPoint.x - pointer.x;
+      const dragY = dragStartPoint.y - pointer.y;
+
+      this.cameras.main.scrollX += dragX / this.cameras.main.zoom;
+      this.cameras.main.scrollY += dragY / this.cameras.main.zoom;
+
+      dragStartPoint.set(pointer.x, pointer.y);
+    });
+  }
+
+  drawGrid() {
+    const cam = this.cameras.main;
+
+    this.gridGraphics.clear();
+    this.gridGraphics.fillStyle(0x000000, 1); // color and alpha
+
+    const startX =
+      Math.floor(cam.worldView.x / this.TILE_SIZE) * this.TILE_SIZE;
+    const endX =
+      Math.ceil((cam.worldView.x + cam.worldView.width) / this.TILE_SIZE) *
+      this.TILE_SIZE;
+
+    const startY =
+      Math.floor(cam.worldView.y / this.TILE_SIZE) * this.TILE_SIZE;
+    const endY =
+      Math.ceil((cam.worldView.y + cam.worldView.height) / this.TILE_SIZE) *
+      this.TILE_SIZE;
+
+    const dotSpacing = 4;
+    const dotLength = 0.4;
+    const dotWidth = 1.2;
+
+    // Vertical dotted lines
+    for (let x = startX; x <= endX; x += this.TILE_SIZE) {
+      for (let y = startY - dotLength; y <= endY - dotLength; y += dotSpacing) {
+        this.gridGraphics.fillRect(
+          x - dotLength / 2,
+          y - dotLength / 2,
+          dotLength,
+          dotWidth,
+        );
+      }
+    }
+
+    // Horizontal dotted lines
+    for (let y = startY; y <= endY; y += this.TILE_SIZE) {
+      for (let x = startX - dotLength; x <= endX - dotLength; x += dotSpacing) {
+        this.gridGraphics.fillRect(
+          x - dotLength / 2,
+          y - dotLength / 2,
+          dotWidth,
+          dotLength,
+        );
+      }
+    }
+  }
+
+  update() {
+    this.drawGrid();
+  }
+}

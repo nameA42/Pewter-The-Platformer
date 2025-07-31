561fa390b560012ef2c7ab290771f9a638105acf
bepratt <bepratt@ucsc.edu>
2025-07-31 14:40:44 -0700

    added simple copy/paste tile placement and keyboard controls for camera motion

--- src/phaser/editorScene.ts
+++ src/phaser/editorScene.ts
@@ -11,11 +11,20 @@ export class EditorScene extends Phaser.Scene {
   private maxZoomLevel = 10;
   private zoomLevel = 2.25;
 
-
   private minimap!: Phaser.Cameras.Scene2D.Camera;
   private minimapZoom = 0.15;
 
   private scrollDeadzone = 50; // pixels from the edge of the camera view to stop scrolling
+  private scrollSpeed = 10; // pixels per second
+
+  private selectedTileIndex = 0; // index of the tile to place
+
+  // keyboard controls
+  private keyA!: Phaser.Input.Keyboard.Key;
+  private keyD!: Phaser.Input.Keyboard.Key;
+  private keyW!: Phaser.Input.Keyboard.Key;
+  private keyS!: Phaser.Input.Keyboard.Key;
+  private keyShift!: Phaser.Input.Keyboard.Key;
 
   constructor() {
     super({ key: "editorScene" });
@@ -26,6 +35,7 @@ export class EditorScene extends Phaser.Scene {
   create() {
     this.map = this.make.tilemap({ key: "defaultMap" });
 
+    console.log("Map loaded:", this.map);
     const tileset = this.map.addTilesetImage(
       "pewterPlatformerTileset",
       "tileset",
@@ -34,6 +44,7 @@ export class EditorScene extends Phaser.Scene {
       0,
       0,
     )!;
+    // console.log("Tileset added:", this.map);
 
     this.backgroundLayer = this.map.createLayer(
       "Background_Layer",
@@ -41,7 +52,9 @@ export class EditorScene extends Phaser.Scene {
       0,
       0,
     )!;
+    // console.log("LAYER1 added:", this.map);
     this.groundLayer = this.map.createLayer("Ground_Layer", tileset, 0, 0)!;
+    // console.log("LAYER2 added:", this.map);
 
     this.cameras.main.setBounds(
       0,
@@ -96,14 +109,29 @@ export class EditorScene extends Phaser.Scene {
       },
     );
 
+    if (this.input.mouse)
+    {
+      this.input.mouse.disableContextMenu();
+    }
+
     // scrolling
     let isDragging = false;
     let dragStartPoint = new Phaser.Math.Vector2();
 
     this.input.on("pointerdown", (pointer: Phaser.Input.Pointer) => {
-      if (pointer.rightButtonDown()) return; // Optional: allow only left-click
-      isDragging = true;
-      dragStartPoint.set(pointer.x, pointer.y);
+      if (pointer.middleButtonDown()) 
+      {
+        isDragging = true;
+        dragStartPoint.set(pointer.x, pointer.y);
+      }
+      else if (pointer.leftButtonDown()) {
+        const tileX = Math.floor(pointer.worldX / this.TILE_SIZE);
+        const tileY = Math.floor(pointer.worldY / this.TILE_SIZE);
+        this.placeTile(this.groundLayer, tileX, tileY, this.selectedTileIndex);
+      }
+      else if (pointer.rightButtonDown()) {
+        this.selectedTileIndex = this.groundLayer.getTileAtWorldXY(pointer.worldX, pointer.worldY)?.index || 0;
+      }
     });
 
     this.input.on("pointerup", () => {
@@ -129,6 +157,34 @@ export class EditorScene extends Phaser.Scene {
 
       dragStartPoint.set(pointer.x, pointer.y);
     });
+
+    if(this.input.keyboard) {
+      this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+      this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+      this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+      this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+      this.keyShift = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT);
+    }
+  }
+
+  cameraMotion() {
+    const cam = this.cameras.main;
+    let scrollSpeed = this.scrollSpeed;
+    if (this.keyShift.isDown) {
+      scrollSpeed *= 4;
+    }
+    if (this.keyA.isDown) {
+      cam.scrollX -= scrollSpeed / cam.zoom;
+    }
+    if (this.keyD.isDown) {
+      cam.scrollX += scrollSpeed / cam.zoom;
+    }
+    if (this.keyW.isDown) {
+      cam.scrollY -= scrollSpeed / cam.zoom;
+    }
+    if (this.keyS.isDown) {
+      cam.scrollY += scrollSpeed / cam.zoom;
+    }
   }
 
   drawGrid() {
@@ -183,7 +239,14 @@ export class EditorScene extends Phaser.Scene {
     }
   }
 
+  placeTile(layer: Phaser.Tilemaps.TilemapLayer, x: number, y: number, tileIndex: number) {
+    tileIndex = Phaser.Math.Clamp(tileIndex, 1, layer.tilemap.tilesets[0].total - 1);
+    console.log(`Placing tile at (${x}, ${y}) with index ${tileIndex}`);
+    layer.putTileAt(tileIndex, x, y);
+  }
+
   update() {
     this.drawGrid();
+    this.cameraMotion();
   }
 }
